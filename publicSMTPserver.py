2e8676f1ae0406dd635a0ee2466047033329c684
Rohit <rohythakur@gmail.com>
2016-11-30 21:17:27 +0530

    solved padding

--- publicSMTPserver.py
+++ publicSMTPserver.py
@@ -8,26 +8,26 @@ from email.mime.text import MIMEText
 SMTP_SERVER = 'aspmx.l.google.com'
 SMTP_PORT = 25
 def send_email(sender, recipient):
-""" Send email message """
-msg = MIMEMultipart()
-msg['To'] = recipient
-msg['From'] = sender
-subject = input('Enter your email subject: ')
-msg['Subject'] = subject
-message = input('Enter your email message. Press Enter when
-finished. ')
-part = MIMEText('text', "plain")
-part.set_payload(message)
-msg.attach(part)
-# create smtp session
-session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-session.ehlo()
-#session.set_debuglevel(1)
-# send mail
-session.sendmail(sender, recipient, msg.as_string())
-print("You email is sent to {0}.".format(recipient))
-session.quit()
+    """ Send email message """
+    msg = MIMEMultipart()
+    msg['To'] = recipient
+    msg['From'] = sender
+    subject = input('Enter your email subject: ')
+    msg['Subject'] = subject
+    message = input('Enter your email message. Press Enter when
+    finished. ')
+    part = MIMEText('text', "plain")
+    part.set_payload(message)
+    msg.attach(part)
+    # create smtp session
+    session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    session.ehlo()
+    #session.set_debuglevel(1)
+    # send mail
+    session.sendmail(sender, recipient, msg.as_string())
+    print("You email is sent to {0}.".format(recipient))
+    session.quit()
 if __name__ == '__main__':
-sender = input("Enter sender email address: ")
-recipient = input("Enter recipient email address: ")
-send_email(sender, recipient)
+    sender = input("Enter sender email address: ")
+    recipient = input("Enter recipient email address: ")
+    send_email(sender, recipient)
